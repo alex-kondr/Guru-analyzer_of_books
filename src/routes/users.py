4ddcac83e2c9478479eaa16babe528368383a0cd
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2023-09-15 13:47:55 +0300

    add upload file

--- src/routes/users.py
+++ src/routes/users.py
@@ -1,27 +1,24 @@
-from datetime import datetime, date
-from typing import List
-from fastapi import APIRouter, Depends, status, UploadFile, File, HTTPException, Query, Path
+from fastapi import APIRouter, Depends, UploadFile, File
 
 from sqlalchemy.orm import Session
 
 from src.database.db import get_db
 from src.database.models import User
-from src.repository import users as repository_users
 from src.services.auth import auth_service
-from src.schemas.users import UserDb, UserUpdateModel
-from src.conf import messages
+from src.schemas.users import UserDb
 
 
 router = APIRouter(prefix="/users", tags=["users"])
 
 
-@router.get("/{user_id}", response_model=UserDb)
-async def read_user_by_id(user_id: int = Path(ge=1),
-                     db: Session = Depends(get_db),
-                     _: User = Depends(auth_service.get_current_user)):
-
-    user = await repository_users.get_user_by_id(user_id, db)
-    if user == None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=messages.NOT_FOUND)
+@router.get("/me", response_model=UserDb)
+async def read_user_by_id(user: User = Depends(auth_service.get_current_user)):
     return user
+
+
+@router.post("/files/")
+async def create_file(file: UploadFile = File(),
+                      _: User = Depends(auth_service.get_current_user)):
+    return {"file_size": file.size,
+            "file_name": file.filename,
+            "file": file.file}
