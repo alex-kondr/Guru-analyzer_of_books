b50253ab14e6ac364e40ab1e47b8571a5ab48e4f
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2023-09-18 11:05:25 +0300

    bugfix: table, message

--- src/conf/messages.py
+++ src/conf/messages.py
@@ -1,16 +1,14 @@
 # messages for
 
 # --> src.routes.auth.py && src.routes.contacts.py
-EMAIL_NOT_CONFIRMED = "Email not confirmed"
 INVALID_EMAIL = "Invalid email"
 INVALID_PASSWORD = "Invalid password"
 ACCOUNT_ALREADY_EXISTS = "Account already exists"
-USER_SUCCESSFULLY_CREATED = "User successfully created. Check your email for confirmation."
+USER_SUCCESSFULLY_CREATED = "User successfully created"
 INVALID_REFRESH_TOKEN = "Invalid refresh token"
 VERIFICATION_ERROR = "Verification error"
 NOT_FOUND = "Not Found"
 INVALID_SCOPE_FOR_TOKEN = "Invalid scope for token"
-INVALID_TOKEN_FOR_EMAIL_VERIFICATION = "Invalid token for email verification"
 COULD_NOT_VALIDATE_CREDENTIALS = "Could not validate credentials"
 FORBIDDEN = "Operation forbidden"
 USER_NOT_FOUND = 'User not found'
