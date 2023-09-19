c08d118c0f8df43df585d3a8072ba5a7495b3f37
VadymBoyko <109297726+VadymBoyko@users.noreply.github.com>
2023-09-19 12:42:49 +0300

    change chat route

--- src/routes/chats.py
+++ src/routes/chats.py
@@ -8,7 +8,7 @@ from src.database.db import get_db
 from src.database.models import User
 from src.conf import constants
 from src.services.auth import auth_service
-from src.schemas.chats import ChatHistoryResponse, ChatResponse
+from src.schemas.chats import ChatHistoryResponse, ChatResponse, ChatQuestion
 from src.repository import chats as repository_chats
 from src.repository import files as repository_files
 from src.model.model import answer_generate
@@ -24,11 +24,11 @@ async def read_chat_by_document_id(document_id: int,
 
 
 @router.post("/", name="Ask a question to file", response_model=ChatResponse)
-async def ask_question(document_id: int,
-                       question: str,
+async def ask_question(body: ChatQuestion,
                        db: Session = Depends(get_db),
                        current_user: User = Depends(auth_service.get_current_user)):
-
+    document_id = body.document_id
+    question = body.question
     document = await repository_files.get_document_by_id(document_id=document_id,
                                                          user_id=current_user.id,
                                                          db=db)
