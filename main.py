65905b6be8c729c624acc8bc9fe47ab4cb6e406c
alex_kondr <alex_kondr@outlook.com>
2023-09-22 01:08:15 +0300

    add Dockerfile

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def healthchecker(db: Session = Depends(get_db)):
         result = db.execute(text("SELECT 1")).fetchone()
         if result is None:
             raise HTTPException(status_code=500, detail="Database is not configured correctly")
-        return {"message": "Welcome to FastAPI!"}
+        return {"message": "Welcome to Guru-analyzer of books!"}
     except Exception as e:
         print(e)
         raise HTTPException(status_code=500, detail="Error connecting to the database")
