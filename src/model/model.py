7cda9461c9035781bdb8269faed804ed89a6d2d8
alex_kondr <alex_kondr@outlook.com>
2023-09-23 13:05:38 +0300

    add import en_core_web_sm

--- src/model/model.py
+++ src/model/model.py
@@ -137,7 +137,7 @@ async def document_summary_generate(document_id: int, sentences_count: int = 5):
     log.log(logging.DEBUG, "try spacy")
 
     log.log(logging.DEBUG, "try try spacy")
-    nlp = spacy.load("en_core_web_sm")
+    # nlp = spacy.load("en_core_web_sm")
     log.log(logging.DEBUG, "load nlp")
     nlp = en_core_web_sm.load()
     log.log(logging.DEBUG, "load spacy")
