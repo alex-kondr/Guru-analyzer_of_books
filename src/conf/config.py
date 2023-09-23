67027c3984da3190fa256f0c005389fe79a91c12
alex_kondr <alex_kondr@outlook.com>
2023-09-23 12:32:10 +0300

    test changed Dockerfile

--- src/conf/config.py
+++ src/conf/config.py
@@ -2,8 +2,10 @@ import os
 
 from pydantic import BaseSettings
 
+
 os.system("python3 -m spacy download en_core_web_sm")
 os.system("py -m spacy download en_core_web_sm")
+
 
 class Settings(BaseSettings):
     sqlalchemy_database_url: str = "postgresql+psycopg2://user:password@localhost:5432/postgres"
